1cb42f78d69583cbf4db8b1fd78f926047713bda
Sharroll Smith <sharrollsmith@angrypagonagames.com>
2023-09-18 17:34:29 -0500

    Update character animation instance
    - Updated the Anim Blueprint BP_ExpeditionistCharacter.uasset
    - Added new file CharacterAnimInstanc.cpp to handle character animation logic
    - Implemented functions to calculate ground speed, air speed, movement status, and falling status in CharacterAnimInstanc.cpp
    - Created CharacterAnimInstanc.h header file with necessary declarations

---
+++ Source/Expeditionist/Private/AnimInstance/CharacterAnimInstanc.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimInstance/CharacterAnimInstanc.h"
+#include "Expeditionist/ExpeditionistCharacter.h"
+#include "Components/CustomMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UCharacterAnimInstanc::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	ExpeditionistCharacter = Cast<AExpeditionistCharacter>(TryGetPawnOwner());
+	if (ExpeditionistCharacter)
+	{
+		CustomMovementComponent= ExpeditionistCharacter->GetCustomMovementComponent();
+	}
+}
+
+void UCharacterAnimInstanc::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if(!ExpeditionistCharacter || !CustomMovementComponent) return;
+	GetGroundSpeed();
+	GetAirSpeed();
+	GetShouldMove();
+	GetIsFalling();
+}
+
+void UCharacterAnimInstanc::GetGroundSpeed()
+{
+	GroundSpeed = UKismetMathLibrary::VSizeXY(ExpeditionistCharacter->GetVelocity());
+	
+}
+
+void UCharacterAnimInstanc::GetAirSpeed()
+{
+	AirSpeed= ExpeditionistCharacter->GetVelocity().Z;
+}
+
+void UCharacterAnimInstanc::GetShouldMove()
+{
+	bShouldMove=
+	CustomMovementComponent->GetCurrentAcceleration().Size()>0&&
+	GroundSpeed > 5.f &&
+	!bIsFalling;
+}
+
+void UCharacterAnimInstanc::GetIsFalling()
+{
+	bIsFalling= CustomMovementComponent->IsFalling();
+}
---
+++ Source/Expeditionist/Public/AnimInstance/CharacterAnimInstanc.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CharacterAnimInstanc.generated.h"
+
+class AExpeditionistCharacter;
+class UCustomMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class EXPEDITIONIST_API UCharacterAnimInstanc : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY()
+	AExpeditionistCharacter* ExpeditionistCharacter;
+
+	UPROPERTY()
+	UCustomMovementComponent* CustomMovementComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta= (AllowPrivateAccess = "true"))
+	float AirSpeed;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta= (AllowPrivateAccess = "true"))
+	bool bShouldMove;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reference", meta= (AllowPrivateAccess = "true"))
+	bool bIsFalling;
+	
+	void GetGroundSpeed();
+	void GetAirSpeed();
+	void GetShouldMove();
+	void GetIsFalling();
+};
